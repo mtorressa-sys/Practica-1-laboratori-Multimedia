838b90dc793188e5aaa1e7afc1df16c08d805648
Neus <neusgisbert20@gmail.com>
2026-04-22 13:07:02 +0200

    canvi cartes

--- P100 PARELLES 25-26 preprojecte/js/p100-memoria.js
+++ P100 PARELLES 25-26 preprojecte/js/p100-memoria.js
@@ -1,34 +1,44 @@
 var ampladaCarta, alcadaCarta;
 var separacioH=20, separacioV=20;
-var nFiles=1, nColumnes=1;
+var nFiles=4, nColumnes=4;
 
 var jocCartes = [
-    'carta14', 
+    'carta1','carta1',
+    'carta2','carta2',
+    'carta3','carta3',
+    'carta4','carta4',
+    'carta5','carta5',
+    'carta6','carta6',
+    'carta7','carta7',
+    'carta8','carta8'
 ];
-
 
 $(function(){
     var f, c, carta;
-    f=1;
-    c=1;
-
- 
 
     ampladaCarta=$(".carta").width(); 
     alcadaCarta=$(".carta").height();
-    // mida del tauler
+
+    // mida del tauler adaptada a files i columnes
     $("#tauler").css({
-        "width" : "120px",
-        "height": "160px"
+        "width" : (nColumnes*(ampladaCarta+separacioH)+separacioH)+"px",
+        "height": (nFiles*(alcadaCarta+separacioV)+separacioV)+"px"
     });
 
-    carta=$("#f"+f+"c"+c);
-    carta.css({
-        "left" :  ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px",
-        "top"  :  ((f-1)*(alcadaCarta+separacioV) +separacioV)+"px"
-    });
-    carta.find(".davant").addClass(jocCartes.pop());
-   
+    // posicionar cartes segons files i columnes
+    for(f=1; f<=nFiles; f++){
+        for(c=1; c<=nColumnes; c++){
+            carta=$("#f"+f+"c"+c);
+
+            carta.css({
+                "left" : ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px",
+                "top"  : ((f-1)*(alcadaCarta+separacioV)+separacioV)+"px"
+            });
+
+            carta.find(".davant").addClass(jocCartes.pop());
+        }
+    }
+
     $(".carta").on("click",function(){
         $(this).toggleClass("carta-girada");
     });
