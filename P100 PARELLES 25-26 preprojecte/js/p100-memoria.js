5f032599ec696ce3526909f5da089033473910d8
Marc <mtorressa@edu.tecnocampus.cat>
2026-05-01 17:16:27 +0200

    apartat 1

--- P100 PARELLES 25-26 preprojecte/js/p100-memoria.js
+++ P100 PARELLES 25-26 preprojecte/js/p100-memoria.js
@@ -1,6 +1,9 @@
 var ampladaCarta, alcadaCarta;
 var separacioH=20, separacioV=20;
-var nFiles=4, nColumnes=4;
+
+//modifica per canviar la mida del taulell
+var nFiles=4
+var nColumnes=4;
 
 var jocCartes = [
     'carta1',  'carta2',  'carta3',  'carta4',
@@ -12,57 +15,84 @@ var jocCartes = [
     'carta25', 'carta26'
 ];
 
-$(function(){
-    var f, c, carta;
+function barreja(arr) {
+    var a = arr.slice(); // còpia per no modificar l'original
+    for (var i = a.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i]; a[i] = a[j]; a[j] = tmp;
+    }
+    return a;
+}
 
-    // barreja cartes
-    jocCartes.sort(function() { return 0.5 - Math.random() });
+function generaTauler() {
 
-    // cartes necessaries
-    var seleccionades = jocCartes.slice(0, (nFiles * nColumnes) / 2);
+    // Barreja i selecciona les parelles necessàries
+    var totalCartes     = nFiles * nColumnes;
+    var barrejades      = barreja(jocCartes);
+    var seleccionades   = barrejades.slice(0, totalCartes / 2);
+    var ambParelles     = barreja(seleccionades.concat(seleccionades));
 
-    // parelles i seleccionades
-    var cartesSeleccionades = seleccionades.concat(seleccionades);
-    cartesSeleccionades.sort(function() { return 0.5 - Math.random() });
+    // Agafa mides de la carta (existeix al CSS)
+    // Fem servir una carta temporal per llegir-ne les mides
+    var cartaTemp = $('<div class="carta" style="visibility:hidden"></div>').appendTo("body");
+    ampladaCarta  = cartaTemp.width();
+    alcadaCarta   = cartaTemp.height();
+    cartaTemp.remove();
 
-    ampladaCarta=$(".carta").width(); 
-    alcadaCarta=$(".carta").height();
-
-    // mida del tauler
+    // Mida del tauler
     $("#tauler").css({
-        "width" : (nColumnes*(ampladaCarta+separacioH)+separacioH)+"px",
-        "height": (nFiles*(alcadaCarta+separacioV)+separacioV)+"px"
+        "width" : (nColumnes * (ampladaCarta + separacioH) + separacioH) + "px",
+        "height": (nFiles    * (alcadaCarta  + separacioV) + separacioV) + "px"
     });
 
-    //buidar taulell
+    // Buidar tauler
     $("#tauler").empty();
-    
-    // CREAR totes les cartes
-    for(f=1; f<=nFiles; f++){
-        for(c=1; c<=nColumnes; c++){
+
+    // Crear cartes
+    var index = 0;
+    for (var f = 1; f <= nFiles; f++) {
+        for (var c = 1; c <= nColumnes; c++) {
+
+            var idCarta = "f" + f + "c" + c;
 
             $("#tauler").append(
-                '<div class="carta" id="f'+f+'c'+c+'">'+
-                    '<div class="cara davant"></div>'+
-                    '<div class="cara darrera"></div>'+
+                '<div class="carta" id="' + idCarta + '">' +
+                    '<div class="cara davant"></div>' +
+                    '<div class="cara darrera"></div>' +
                 '</div>'
             );
 
-            carta=$("#f"+f+"c"+c);
+            var $carta = $("#" + idCarta);
 
-            carta.css({
-                "left" : ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px",
-                "top"  : ((f-1)*(alcadaCarta+separacioV)+separacioV)+"px"
+            $carta.css({
+                "left": ((c - 1) * (ampladaCarta + separacioH) + separacioH) + "px",
+                "top" : ((f - 1) * (alcadaCarta  + separacioV) + separacioV) + "px"
             });
 
-            //parelles seleccionades
-            var finalistes = cartesSeleccionades.pop();
-            carta.find(".davant").addClass(finalistes);
+            // Assigna la figura (classe CSS) a la cara del davant
+            $carta.find(".davant").addClass(ambParelles[index]);
+            // Guarda el nom de la figura com a data per comparar després
+            $carta.data("figura", ambParelles[index]);
+
+            index++;
         }
     }
 
-    $(".carta").on("click",function(){
+    $(".carta").on("click", function () {
         $(this).toggleClass("carta-girada");
     });
+}
 
+$(function () {
+
+    // Botó iniciar: llegeix el select i genera el tauler
+    $("#btn-iniciar").on("click", function () {
+        var valor = $("#select-nivell").val().split("-");
+        nFiles    = parseInt(valor[0]);
+        nColumnes = parseInt(valor[1]);
+        generaTauler();
+    });
+
+    // Genera el tauler per defecte amb els valors inicials de 1a
+    generaTauler();
 });
